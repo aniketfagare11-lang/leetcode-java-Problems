78ce8b2439b4bff42d356f8b627b45d3bc92866f
Aniketfagare11-lang <aniketfagare11@gmail.com>
2026-02-23 22:03:57 +0530

    Problem -2011

---
+++ Day15/Operations.java
@@ -0,0 +1,24 @@
+package Day15;
+
+public class Operations {
+    static int finalValueAfterOperations(String[] operations){
+        int x=0;
+        for(int i=0;i<operations.length;i++)
+        {
+            if(operations[i].contains("+"))
+                {
+                    x++;
+                } 
+            else{
+                  x--;
+            }  
+        }
+        return x;
+    }
+
+    public static void main(String[] args) {
+        String[] operations = {"--X","X++","X++"};
+        int ans=finalValueAfterOperations(operations);
+        System.out.println(ans); 
+    }
+}
