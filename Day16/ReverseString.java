25e7fe2485316b76a9f25474efac567ff4acdce1
Aniketfagare11-lang <aniketfagare11@gmail.com>
2026-02-24 22:17:07 +0530

    Problem 344

---
+++ Day16/ReverseString.java
@@ -0,0 +1,17 @@
+package Day16;
+import java.util.Arrays;
+public class ReverseString {
+ static void reverseString(char[] s){
+    for(int i = 0; i < s.length / 2; i++)
+        {
+            char temp = s[i];
+            s[i] = s[s.length - 1 - i];
+            s[s.length - 1 - i] = temp;
+        }
+     }
+   public static void main(String[] args) {
+    char [] s = {'h','e','l','l','o'};
+    reverseString(s);
+    System.out.println(Arrays.toString(s));
+}
+}
